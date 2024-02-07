2f1e030ee7fc2469c7e69e5d04e2c87cab0d095c
stephen mehi <mehi.j.stephen@gmail.com>
2024-02-07 15:25:54 -0800

    wip

--- Services/PhoenixProxy.cs
+++ Services/PhoenixProxy.cs
@@ -99,7 +99,7 @@ public class PhoenixProxy : IDisposable
         _connectAction =
             new Func<Task<IDisposableIOBus>>(() =>
                 _connectFactory.ConnectAsync(
-                "192.168.1.100",
+                "10.1.129.100",
                 TimeSpan.FromSeconds(3),
                 TimeSpan.FromSeconds(1),
                 new Dictionary<ushort, AnalogInputConfig>(),
